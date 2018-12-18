85e7417c4b0dfd657afa63e75aa4fcffe519ce32
swity001 <43563298+swity001@users.noreply.github.com>
2018-12-18 10:48:08 +0700

    Add files via upload

---
+++ module/log.py
@@ -0,0 +1,37 @@
+import os, sys
+
+print ("\033[1;32mMasukan UserName&Password Mank :3")
+print ("\033[1;31;1mKalo Gak Tau Pm Swity 089699989010")
+print ("\033[1;31;1mLuBuriqGwGans tq :3")
+username = 'Swity'      
+password = 'Ganteng'
+
+def restart():
+	ngulang = sys.executable
+	os.execl(ngulang, ngulang, *sys.argv)
+
+def main():
+	uname = raw_input("username : ")
+	if uname == username:
+		pwd = raw_input("password : ")
+
+		if pwd == password:
+			print "\n\033[1;34mHello Welcome To Tools CyberLayersTeam", 
+			sys.exit()
+
+		else:
+			print "\n\033[1;36mSorry Invalid Password !!!\033[00m"
+			print "Back Login\n"
+			restart()
+
+	else:
+		print "\n\033[1;36mSorry Invalid Username !!!\033[00m"
+		print "Back Login\n"
+		restart()
+
+try:
+	main()
+except KeyboardInterrupt:
+	os.system('clear')
+	restart()
+    
